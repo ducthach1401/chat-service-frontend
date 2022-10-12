db179ae7724767d2000ba81dd7d25bce4bd1ecaa
ducthach <ducthach.1401@gmail.com>
2022-10-12 09:15:08 +0000

    add scroll

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -35,7 +35,7 @@ export class Home extends React.Component<any, any> {
         (this.state.userId === message.send_user_id &&
           this.state.meId === message.receive_user_id)
       ) {
-        this.renderMessage(message.data, message.is_me);
+        this.renderMessage(message.data, message.is_me, false);
       }
       $("#messages").animate({ scrollTop: 20000000 }, "slow");
     });
@@ -100,13 +100,27 @@ export class Home extends React.Component<any, any> {
     return socket;
   }
 
-  renderMessage(message: string, myMessage: boolean) {
-    if (myMessage) {
-      $("#messages").append(`<div class="my-message"><p>${message}</p></div>`);
+  renderMessage(message: string, myMessage: boolean, isFirst: boolean) {
+    if (isFirst) {
+      if (myMessage) {
+        $("#messages").prepend(
+          `<div class="my-message"><p>${message}</p></div>`
+        );
+      } else {
+        $("#messages").prepend(
+          `<div class="your-message"><p>${message}</p></div>`
+        );
+      }
     } else {
-      $("#messages").append(
-        `<div class="your-message"><p>${message}</p></div>`
-      );
+      if (myMessage) {
+        $("#messages").append(
+          `<div class="my-message"><p>${message}</p></div>`
+        );
+      } else {
+        $("#messages").append(
+          `<div class="your-message"><p>${message}</p></div>`
+        );
+      }
     }
   }
 
@@ -170,11 +184,36 @@ export class Home extends React.Component<any, any> {
     return users;
   }
 
-  async getMessage(id: string) {
-    $("#messages").text("");
+  setSession(id: string, page: number, limit: number) {
+    sessionStorage.setItem("user_id", id);
+    sessionStorage.setItem("page", String(page));
+    sessionStorage.setItem("limit", String(limit));
+  }
+
+  getSession() {
+    return {
+      user_id: sessionStorage.getItem("user_id") ?? "",
+      page: Number(sessionStorage.getItem("page")),
+      limit: Number(sessionStorage.getItem("limit")),
+    };
+  }
+
+  async getMessage(
+    id: string,
+    page: number = 1,
+    limit: number = 20,
+    scroll: boolean = false
+  ) {
+    if (!scroll) {
+      $("#messages").text("");
+    }
+
     this.setState({ userId: id });
+
+    this.setSession(id, page, limit);
+
     const response = await CallApi(
-      `${API_URL}/api/v1/message/id/${id}?page=1&limit=20`,
+      `${API_URL}/api/v1/message/id/${id}?page=${page}&limit=${limit}`,
       METHOD.Get,
       undefined,
       Cookies.get("access_token")
@@ -189,19 +228,34 @@ export class Home extends React.Component<any, any> {
     }
     const messages = response.data;
     const user = await this.getMe();
-    if (messages.length < 1) {
+    if (messages.length < 1 && !scroll) {
       $("#messages").append(
         `<div id="empty"><img src="../../message.png" alt="empty" /> 
         Bạn chưa có tin nhắn! \n Hãy bắt đầu trò chuyện vui vẻ. </div>`
       );
     }
     messages.map((message: any) => {
-      return this.renderMessage(message.data, message.send_user_id === user.id);
+      return this.renderMessage(
+        message.data,
+        message.send_user_id === user.id,
+        true
+      );
     });
-    $("#messages").animate({ scrollTop: 20000000 }, "slow");
+    if (!scroll) {
+      $("#messages").animate({ scrollTop: 20000000 }, "slow");
+    }
+  }
+
+  async loadMore() {
+    const scroll = document.getElementById("messages")?.scrollTop;
+    if (scroll! < 100) {
+      const data = this.getSession();
+      await this.getMessage(data.user_id, data.page + 1, data.limit, true);
+    }
   }
 
   render(): React.ReactNode {
+    let init = 0;
     return (
       <div className="container-fluid">
         <div className="row">
@@ -209,7 +263,15 @@ export class Home extends React.Component<any, any> {
             <div id="user"></div>
           </div>
           <div className="col-9 p-0">
-            <div id="messages"></div>
+            <div
+              id="messages"
+              onScroll={() => {
+                if (init) {
+                  this.loadMore();
+                }
+                init++;
+              }}
+            ></div>
             <div id="input-message">
               <textarea
                 name="message"
